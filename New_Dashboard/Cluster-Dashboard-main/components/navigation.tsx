c9142d8d51d42a7260c0e17085da8f813d9ca680
Rachit Srivastava <rachitsrivastava76@gmail.com>
2025-09-26 07:22:44 +0530

    added api

--- New_Dashboard/Cluster-Dashboard-main/components/navigation.tsx
+++ New_Dashboard/Cluster-Dashboard-main/components/navigation.tsx
@@ -32,7 +32,7 @@ export function Navigation({ setIsLoggedInState }: { setIsLoggedInState: (isLogg
 
   // 🔹 Check session on mount
   useEffect(() => {
-    fetch("solana-cluster.up.railway.app/me", { credentials: "include" })
+    fetch("https://solana-cluster.up.railway.app//me", { credentials: "include" })
       .then((res) => {
         const loggedIn = res.ok
         setIsLoggedIn(loggedIn)
@@ -45,7 +45,7 @@ export function Navigation({ setIsLoggedInState }: { setIsLoggedInState: (isLogg
   }, [setIsLoggedInState])
 
   const handleLogout = async () => {
-    await fetch("solana-cluster.up.railway.app/logout", {
+    await fetch("https://solana-cluster.up.railway.app//logout", {
       method: "POST",
       credentials: "include"
     })
@@ -88,7 +88,7 @@ export function Navigation({ setIsLoggedInState }: { setIsLoggedInState: (isLogg
       return
     }
     try {
-      const res = await fetch("solana-cluster.up.railway.app/signup", {
+      const res = await fetch("https://solana-cluster.up.railway.app//signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
@@ -120,7 +120,7 @@ export function Navigation({ setIsLoggedInState }: { setIsLoggedInState: (isLogg
       return
     }
     try {
-      const res = await fetch("solana-cluster.up.railway.app/login", {
+      const res = await fetch("https://solana-cluster.up.railway.app//login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
